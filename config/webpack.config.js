2fe61a10c7e0ac0dcf7a43f663a5226d4c6667aa
Weiwei SUN <ww.sun@outlook.com>
2015-10-10 16:41:21 +0800

    optimize the webpack behavior

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,14 @@
 var path = require('path');
+var node_modules = path.resolve(__dirname, '../node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 
 module.exports = {
   entry: path.resolve(__dirname, '../src/client/scripts/client.js'),
+  resolve: {
+    alias: {
+      'react': pathToReact
+    }
+  },
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js'
@@ -10,6 +17,7 @@ module.exports = {
   module: {
     loaders: [
       {test: /src(\\|\/).+\.jsx?$/, exclude: /node_modules/, loader: 'babel'}
-    ]
+    ],
+    noParse: [pathToReact]
   }
 };
